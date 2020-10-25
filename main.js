9dbcae4f1a4d213cfb7b4828df004b326ed7a8bf
Damian Tabor <zettie008@gmail.com>
2020-10-25 22:18:40 +0000

    Working on infinite scroll

---
+++ main.js
@@ -0,0 +1,41 @@
+const container = document.getElementById('container');
+const preloader = document.getElementById('preloader');
+const paragraphs = document.querySelectorAll('p');
+
+
+const enableInfiniteScroll = () => {
+
+    const removeItem = (event) => {
+        console.log(event.currentTarget);
+    };
+
+    paragraphs.forEach(paragraph => paragraph.addEventListener('click', removeItem));
+
+    let block_request = false;
+
+    container.addEventListener('scroll', event => {
+        const { scrollTop, clientHeight, scrollHeight } = container;
+        if (scrollTop + clientHeight >= scrollHeight && block_request == false) {
+            block_request = true;
+            preloader.style.display = 'block';
+            // Simulate AJAX request to the server 
+            setTimeout(() => {
+                const paragraph = document.createElement('p');
+                paragraph.textContent = 'NEW PARAGRAPH';
+                paragraph.addEventListener('click', removeItem);
+                container.insertBefore(paragraph, preloader);
+                preloader.style.display = 'none';
+                block_request = false;
+            }, 2000)
+        }
+    })
+};
+
+
+
+
+
+
+
+enableInfiniteScroll();
+
